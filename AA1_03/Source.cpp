2533a2b495ee293f2a482d87aeffd957b671391c
Pau Amigó <pau.amigo@enti.cat>
2022-03-03 22:50:54 +0100

    Created main

--- AA1_03/Source.cpp
+++ AA1_03/Source.cpp
@@ -3,4 +3,17 @@ using namespace std;
 #include "Header.h"
 
 int main() {
+    bool game;
+    do {
+
+        initializeBoard(board);
+        existsCoin(player, move, board);
+        game = gameOver();
+        printBoard(board);
+        if (checkMovement(player, move, board) == true) {
+            movePlayer();
+        }
+        addscore(player, board);
+
+    } while (game == false);
 }
