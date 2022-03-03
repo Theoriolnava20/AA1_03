8a852e66be1d6979537a68eab6b76b9c4f580cb4
Pau Amigó <pau.amigo@enti.cat>
2022-03-03 21:35:30 +0100

    fixed errors

--- AA1_03/Header.h
+++ AA1_03/Header.h
@@ -7,11 +7,10 @@ char coin = 'O';
 char pj = 'P';
 char empty = ' ';
 char board[NUM_ROWS][NUM_COLS];
-int maxStones = (NUM_ROWS * NUM_COLS) * 0.2;
-int maxCoins = (NUM_ROWS * NUM_COLS) * 0.3;
+const int maxStones = (NUM_ROWS * NUM_COLS) * 0.2;
+const int maxCoins = (NUM_ROWS * NUM_COLS) * 0.3;
 int numberStones;
 int numberCoins;
-
 
 enum class Movement {
 	UP,
@@ -23,7 +22,6 @@ enum class Movement {
 struct Player {
 	int score;
 	int position[2];//position[0] sera la X i position[1] sera la Y
-	
 };
 Player player;
 
@@ -33,12 +31,9 @@ int random(int nMin, int nMax)
 }
 
 void initializeBoard() {
-	
-	const int maxStones = (NUM_ROWS * NUM_COLS) * 0.2;
-	const int maxCoins = (NUM_ROWS * NUM_COLS) * 0.3;
 	//NUMBER OF COINS && STONES
-	numberStones = random(1,maxstones);
-	numberCoins = random(1, maxcoins);
+	numberStones = random(1,maxStones);
+	numberCoins = random(1, maxCoins);
 	//Position
 	int randomX = random(0, NUM_ROWS * NUM_COLS);
 	int randomY = random(0, NUM_ROWS * NUM_COLS);
@@ -80,31 +75,46 @@ void initializeBoard() {
 	}
 }
 
-bool checkMovement(Player player, Movement move) {
+bool checkMovement(Player player, Movement move, char board[NUM_ROWS][NUM_COLS]) {
 	if (move == Movement::UP) {
 		if (player.position[1] == 0)
 			return false;
-		else
-			return true;
+		else{
+			if (board[player.position[0]][player.position[1]-1] == stone)
+				return false;
+			else
+				return true
+		}
 	}
 	else if(move == Movement::DOWN) {
 		if (player.position[1] == 9)
 			return false;
-		else
-			return true;
-
+		else {
+			if (board[player.position[0]][player.position[1] + 1] == stone)
+				return false;
+			else
+				return true
+		}
 	}
 	if (move == Movement::LEFT) {
 		if (player.position[0] == 0)
 			return false;
-		else
-			return true;
+		else {
+			if (board[player.position[0]-1][player.position[1]] == stone)
+				return false;
+			else
+				return true
+		}
 	}
 	else if (move == Movement::RIGHT) {
 		if (player.position[0] == 9)
 			return false;
-		else
-			return true;
+		else {
+			if (board[player.position[0] + 1][player.position[1]] == stone)
+				return false;
+			else
+				return true
+		}
 	}
 }
 
@@ -113,7 +123,7 @@ void addscore(Player player, char board[NUM_ROWS][NUM_COLS]) {
 	{
 		if (board[player.position[0]][player.position[1]] == coin) {
 			player.score++;
-			board[player.position[0]][player.position[1]] ==' ')
+			board[player.position[0]][player.position[1]] = empty;
 		}
 	}
 }
@@ -123,9 +133,9 @@ void setPos(){
 }
 
 bool existsCoin(Player player, Movement move, char board[NUM_ROWS][NUM_COLS]) {
-	bool possibleMove=
+	bool possibleMove = checkMovement(player, move, board[NUM_ROWS][NUM_COLS]);
 	if (move == Movement::UP) {
-		
+		if(possibleMove==true)
 	}
 	else if (move == Movement::DOWN) {
 		
